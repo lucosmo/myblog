ee6e168a28e686a3c4854ce57123bdcc65c6da3c
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-02-11 13:26:30 +0000

    added passenger_wsgi required for app deployment

---
+++ passenger_wsgi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from urllib.parse import unquote
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from django.core.wsgi import get_wsgi_application
+
+DJANGO_SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
+
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE  
+def application(environ, start_response):
+    environ["PATH_INFO"] = unquote(environ["PATH_INFO"]).encode('utf-8').decode('iso-8859-1')
+    _application = get_wsgi_application()
+    return _application(environ, start_response)

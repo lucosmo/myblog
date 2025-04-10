32d0e95bb332b56a177290fe22afd4e00c16632c
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-04-10 10:55:35 +0100

    Fix: adjust blank lines to satisfy Flake8 (E302)

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,6 +10,7 @@ sys.path.append(os.getcwd())
 
 DJANGO_SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
 
+
 def application(environ, start_response):
     environ["PATH_INFO"] = (
         unquote(environ["PATH_INFO"]).encode("utf-8").decode("iso-8859-1")
